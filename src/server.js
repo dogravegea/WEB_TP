2da194dee41ef26db8b919126b0a42007ac334a3
Dorian GRAVEGEAL <dogravegea@iut.local>
2018-11-30 09:34:38 +0100

    Ajout variable environnement

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express()
-const models = require('./models/index');
+//const models = require('./models/index');
 
 console.log("ok")
 console.log("modification 2")
@@ -14,32 +14,39 @@ app.use(bodyParser.json())
 // Add a bit of logging
 app.use(morgan('short'))
 
-// Get all the users defined
 app.get('/', function (req, res) {
   models.User.findAll()
-    .then((users) => {
-      res.json(users)
+    .then((users) => {// Get all the users defined
+      res.send("Hello world")//app.get('/', function (req, res) {
     })
 })
 
-// Add a new user to the database
-app.post('/', function(req, res) {
-  models.User.create({
-    username: req.body.username
-  })
-    .then(() => {
-      res.send('User added !')
-    })
-})
+//      res.json(users)
+//    })
+//})
+//
+//// Add a new user to the database
+//app.post('/', function(req, res) {
+//  models.User.create({
+//    username: req.body.username
+//  })
+//    .then(() => {
+//      res.send('User added !')
+//    })
+//})
 
 // Synchronize models
-models.sequelize.sync().then(function() {
-  /**
-   * Listen on provided port, on all network interfaces.
-   * 
-   * Listen only when database connection is sucessfull
-   */
-  app.listen(3000, function() {
+//models.sequelize.sync().then(function() {
+//  /**
+//   * Listen on provided port, on all network interfaces.
+//   * 
+//   * Listen only when database connection is sucessfull
+//   */
+//  app.listen(process.env.PORT, function() {
+//    console.log('Express server listening on port 3000');
+//  });
+//});
+
+app.listen(process.env.PORT, function () {
     console.log('Express server listening on port 3000');
-  });
 });
